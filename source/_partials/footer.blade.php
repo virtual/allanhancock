3b049314fd3a0b404fcedeb621e0f7966e36c775
virtual <jeanine.mt@gmail.com>
2018-12-05 19:25:30 +0000

    footer rebuild

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -3,8 +3,8 @@
     <div class="inner">
         <div class="row">
      
-        <div class="col-12 col-md-6 col-lg-4">
-          <h3>Our Mission</h3>  
+        <div class="col-12 col-md-6 col-lg-3">
+          <h2>Our Mission</h2>  
           <p>Allan Hancock College provides quality 
               educational opportunities that enhance 
               student learning and the creative, 
@@ -12,42 +12,58 @@
               of our diverse community.</p>
           
         </div> 
-        <div class="col-12 col-md-6 col-lg-4">
-          <ul class="list-unstyled list-inline inline-list-padded footer-social">
-            <li>
-               <a title="Allan Hancock College on Facebook" href="http://www.facebook.com/allanhancockcollege"><span class="fab fa-2x fa-facebook-f" aria-hidden="true"></span> <span class="sr-only">Facebook</span></a>
-            </li>
-            <li>
-               <a title="Allan Hancock College on Twitter" href="http://twitter.com/HancockCollege"><span class="fab fa-2x fa-twitter" aria-hidden="true"></span> <span class="sr-only">Twitter</span></a>
-            </li>
-            <li>
-              <a title="Allan Hancock College on Instagram" href="https://www.instagram.com/allanhancockcollege/"><span class="fab fa-2x fa-instagram" aria-hidden="true"></span> <span class="sr-only">Instagram</span></a>
-           </li>
-           <li>
-              <a title="Allan Hancock College on Flckr" href="https://www.flickr.com/photos/126591732@N06/albums"><span class="fab fa-2x fa-flickr" aria-hidden="true"></span> <span class="sr-only">Flckr</span></a>
-           </li>
-            <li>
-               <a title="Allan Hancock College on Youtube" href="http://www.youtube.com/user/AllanHancockCollege"><span class="fab fa-2x fa-youtube" aria-hidden="true"></span> <span class="sr-only">Youtube</span></a>
-            </li>
-            
-         </ul>
-          
-        </div>
+       
 
-        <div class="col-12 col-lg-4 footer-column-1">
-          <div class="links">
-
-
-              <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/public_affairs/privacy_policy.php">Privacy Policy</a></li>
-                <li><a href="/public_affairs/directories.php">Contact Us</a></li>
-                <li><a href="/human_resources/index.php">Employment</a></li>
-                <li><a href="/public_affairs/college-directory.php">College Directory</a></li>  
-                <li><a href="/lap/accessibility.php">Accessibility</a></li>
-                <li><a href="/complaints/index.php">Complaints</a></li>
-              </ul> 
+        <div class="col-12 col-lg-6 footer-column-1">
+            <h2>Quick Links</h2>  
+          <div class="row links">
+              <div class="col-12 col-sm-6">
+                  <ul>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/public_affairs/privacy_policy.php">Privacy Policy</a></li>
+                      <li><a href="/public_affairs/directories.php">Contact Us</a></li>
+                      <li><a href="/human_resources/index.php">Employment</a></li>
+                      <li><a href="/public_affairs/college-directory.php">College Directory</a></li>  
+                      <li><a href="/lap/accessibility.php">Accessibility</a></li>
+                      <li><a href="/complaints/index.php">Complaints</a></li>
+                    </ul> 
+                </div>
+                <div class="col-12 col-sm-6">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/public_affairs/privacy_policy.php">Privacy Policy</a></li>
+                        <li><a href="/public_affairs/directories.php">Contact Us</a></li>
+                        <li><a href="/human_resources/index.php">Employment</a></li>
+                        <li><a href="/public_affairs/college-directory.php">College Directory</a></li>  
+                        <li><a href="/lap/accessibility.php">Accessibility</a></li>
+                        <li><a href="/complaints/index.php">Complaints</a></li>
+                      </ul> 
+                </div>
           </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-3">
+              <h2>Follow Hancock</h2>  
+            <ul class="list-unstyled list-inline inline-list-padded footer-social">
+              <li>
+                 <a title="Allan Hancock College on Facebook" href="http://www.facebook.com/allanhancockcollege"><span class="fab fa-2x fa-facebook-f" aria-hidden="true"></span> <span class="sr-only">Facebook</span></a>
+              </li>
+              <li>
+                 <a title="Allan Hancock College on Twitter" href="http://twitter.com/HancockCollege"><span class="fab fa-2x fa-twitter" aria-hidden="true"></span> <span class="sr-only">Twitter</span></a>
+              </li>
+              <li>
+                <a title="Allan Hancock College on Instagram" href="https://www.instagram.com/allanhancockcollege/"><span class="fab fa-2x fa-instagram" aria-hidden="true"></span> <span class="sr-only">Instagram</span></a>
+             </li>
+             <li>
+                <a title="Allan Hancock College on Flckr" href="https://www.flickr.com/photos/126591732@N06/albums"><span class="fab fa-2x fa-flickr" aria-hidden="true"></span> <span class="sr-only">Flckr</span></a>
+             </li>
+              <li>
+                 <a title="Allan Hancock College on Youtube" href="http://www.youtube.com/user/AllanHancockCollege"><span class="fab fa-2x fa-youtube" aria-hidden="true"></span> <span class="sr-only">Youtube</span></a>
+              </li>
+              
+           </ul>
+            
+           <p><span id="directedit">&nbsp;</span></p>
           </div>
         
       </div>
