7294a0aa2408ead12889eba0c93b8a99aaba18de
virtual <jeanine.mt@gmail.com>
2018-12-19 16:29:52 +0000

    fix news image links

--- source/_partials/article.blade.php
+++ source/_partials/article.blade.php
@@ -1,5 +1,5 @@
 <article class="news-article"> 
-  <div class="image-container">@if(!empty($img)) <a href="{{ $link }}"><img src="{{ $page->baseUrl }}{{$img}}" alt="{{$alt}}"></a>@endif</div>  
+  <div class="image-container">@if(!empty($img)) <a href="{{ $link }}"><img src="{{$img}}" alt="{{$alt}}"></a>@endif</div>  
   <div class="article-info-container">
       @if(!empty($datetime))<p class="datetime"><time datetime="{{$datetime}}"><?php echo date("F j, Y", strtotime($datetime)) ?> </time></p>@endif
       <h4><a href="{{ $link }}">{{ $heading }}</a></h4>
