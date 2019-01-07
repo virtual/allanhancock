60ec372eacaa333a3d8cb42ed2a5b70fb53d27d2
virtual <jeanine.mt@gmail.com>
2019-01-07 17:30:12 +0000

    enroll1 cta card snippet

--- source/enroll1.blade.php
+++ source/enroll1.blade.php
@@ -9,7 +9,7 @@ https://virtual.github.io/allanhancock/resources/img/stock/alexis-brown-85793-un
 @endsection
 
 
-@section('breadcrumb')
+@section('breadcrumb2')
 <ol>
     <li><a href="#">Home</a></li>
     <li><a href="#">Templates</a></li>
@@ -19,45 +19,10 @@ https://virtual.github.io/allanhancock/resources/img/stock/alexis-brown-85793-un
  
  
 @section('content') 
-@include('_partials/headers')
+  <h2 class="pt-4 text-center">What type of student are you?</h2>
+  <p class="pb-3 text-center">Choose from below and find out the next steps to apply and start taking classes at AHC.</p>
 
-@include('_partials/cardgrid')
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut feugiat ultricies mauris ac efficitur. Donec.</p>
-
-
- 
-      <h3><em>College Now!</em> Requirements:
-      </h3>
-      
-      <ul>
-         
-         <li>Be at least junior or senior high school status.</li>
-         
-         <li>Reside and be enrolled in Santa Barbara or San Luis Obispo county high schools or
-            home schools.
-         </li>
-         
-         <li>Students who have previously attended AHC must have a 2.0 cumulative GPA at AHC to
-            continue in the program.
-         </li>
-         
-         <li>Meet with their high school counselor to discuss eligibility, academic requirements,
-            and the rigor of college coursework.
-         </li>
-         
-         <li>Complete the <em>College Now!</em> Petition for Enrollment form; and obtain necessary signatures (high school principal/designee
-            and parent/guardian).
-         </li>
-         
-         <li>Enroll in a maximum of six (6) units per semester.</li>
-         
-         <li>Submit a completed AHC <em>College Now!</em> Petition for Enrollment form and unofficial high school transcripts prior to enrollment
-            to the AHC Admissions &amp; Records office or at the Santa Ynez Valley, VAFB, or Lompoc
-            Valley Center offices.
-         </li>
-         
-      </ul>
-       
+  @include('_partials/cardgrid')
 @endsection
   
 
