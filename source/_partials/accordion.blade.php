39250ebde2e4f1b443b06bb05692fab95f0412b6
virtual <jeanine.mt@gmail.com>
2019-01-25 16:33:25 +0000

    template 1 accordion

--- source/_partials/accordion.blade.php
+++ source/_partials/accordion.blade.php
@@ -1,44 +1,45 @@
+<!-- see shoreline.edu for random id & snippet implementation -->
 <div class="accordion" id="uniqueID">
     <div class="accordion-card">
       <div class="accordion-header" id="headingOne">
         <h3>
-          <button class="btn btn-toggle" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-            Collapsible Group Item #1
+          <button class="btn btn-toggle more-arrow" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+            1. Complete a FREE Online Admission Application
           </button>
         </h3>
       </div>
   
       <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
         <div class="accordion-body">
-            @include('_partials.table-stacked')
+            @include('_partials.nav-tab1')
         </div>
       </div>
     </div>
     <div class="accordion-card">
       <div class="accordion-header" id="headingTwo">
         <h3>
-          <button class="btn btn-toggle collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
+          <button class="btn btn-toggle more-arrow collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            2. Apply for Financial Aid
           </button>
         </h3>
       </div>
       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
         <div class="accordion-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          @include('_partials.nav-tab2')
         </div>
       </div>
     </div>
     <div class="accordion-card">
       <div class="accordion-header" id="headingThree">
         <h3>
-          <button class="btn btn-toggle collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Collapsible Group Item #3
+          <button class="btn btn-toggle more-arrow collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            3. Complete the Online Orientation
           </button>
         </h3>
       </div>
       <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
         <div class="accordion-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          @include('_partials.nav-tab3')
         </div>
       </div>
     </div>
--- source/template1.blade.php
+++ source/template1.blade.php
@@ -23,43 +23,15 @@ mew
  
 @section('content') 
 
-      <p class="text-primary lead"><strong>Your journey starts here.</strong></p>
+      <p class="text-primary lead">Your journey starts here.</p>
  
       @include ('_partials/accordion')
  
-      <h3><em>College Now!</em> Requirements:
-      </h3>
+      <h2>2018-2019</h2>
+      <p><strong>SUMMER 2018</strong> <br />Admission Application: Oct 2, 2017 <br />Online Class Search: April 9, 2018 <br />Priority Registration begins: April 30, 2018 <br />Classes begin: June 11, 2018</p>
+      <p><strong>FALL 2018</strong> <br />Admission Application: Oct 2, 2017 <br />Online Class Search: April 9, 2018 <br />Priority Registration begins: April 30, 2018 <br />Classes begin: August 2, 2018</p>
       
-      <ul>
-         
-         <li>Be at least junior or senior high school status.</li>
-         
-         <li>Reside and be enrolled in Santa Barbara or San Luis Obispo county high schools or
-            home schools.
-         </li>
-         
-         <li>Students who have previously attended AHC must have a 2.0 cumulative GPA at AHC to
-            continue in the program.
-         </li>
-         
-         <li>Meet with their high school counselor to discuss eligibility, academic requirements,
-            and the rigor of college coursework.
-         </li>
-         
-         <li>Complete the <em>College Now!</em> Petition for Enrollment form; and obtain necessary signatures (high school principal/designee
-            and parent/guardian).
-         </li>
-         
-         <li>Enroll in a maximum of six (6) units per semester.</li>
-         
-         <li>Submit a completed AHC <em>College Now!</em> Petition for Enrollment form and unofficial high school transcripts prior to enrollment
-            to the AHC Admissions &amp; Records office or at the Santa Ynez Valley, VAFB, or Lompoc
-            Valley Center offices.
-         </li>
-         
-      </ul>
-      
-@include('_partials/headers')
+ 
        
 @endsection
   
