a7c0c5976a7233c4d4a0f8894da0b33eeb7a2189
virtual <jeanine.mt@gmail.com>
2019-02-18 18:24:41 +0000

    news and events animations

--- source/index.blade.php
+++ source/index.blade.php
@@ -43,6 +43,13 @@
     var heroClass = showRandomHero($('.hero-images .homepage-banner').length)
     $('.hero-images .homepage-banner.' + heroClass).show(); 
     $('.hero-images').addClass('initialized');
+
+    // Delay news and events, non-siblings; dynamic for any number of news or events
+    var delay = 0;
+    $(".animated-news-events article").each(function() {
+      $(this).css('animation-delay', delay + 's');
+      delay = delay + .75;
+    });
   });
 </script>
 @endsection
@@ -68,30 +75,28 @@
 </div>
 @endsection
 
-
-@section ('subfeature') 
-@include('_partials.imagebutton', [
-'img' => './resources/img/ctas/cta-01.jpg',
-'color' => '#000',          
-'buttontext' => 'View Programs',
-'link' => '#'
-])
-@include('_partials.imagebutton', [
-'img' => './resources/img/ctas/cta-02.jpg', 
-'buttontext' => 'Apply to Hancock',
-'link' => '#'
-])
+@section ('subfeature')
   @include('_partials.imagebutton', [
-  'img' => './resources/img/ctas/cta-03.jpg', 
+  'img' => './resources/img/ctas/cta-01.jpg',
+  'color' => '#000',
+  'buttontext' => 'View Programs',
+  'link' => '#'
+  ])
+  @include('_partials.imagebutton', [
+  'img' => './resources/img/ctas/cta-02.jpg',
+  'buttontext' => 'Apply to Hancock',
+  'link' => '#'
+  ])
+  @include('_partials.imagebutton', [
+  'img' => './resources/img/ctas/cta-03.jpg',
   'buttontext' => 'Apply for Financial Aid',
   'link' => '#'
   ])
   @include('_partials.imagebutton', [
-  'img' => './resources/img/ctas/cta-04.jpg', 
+  'img' => './resources/img/ctas/cta-04.jpg',
   'buttontext' => 'View Programs & Majors',
   'link' => '#'
   ])
-
 @endsection
 
 @section('campaign') 
@@ -107,10 +112,13 @@
 
 @section('news-events')
 <h2>News and Events <small>What's going on around campus</small></h2>
+<div class="animated-news-events delayAnimation">
 <div class="row">
   <div class="col-12 col-md-6">
     <h3 class="bar-bg">News</h3>
     <div class="article-listing news-row">
+    
+    <!-- Add option for # of news items to show -->
     @include('_partials.article', [
     'heading' => 'Watch Historic Mars Landing Live at Allan Hancock College',
     'desc' => 'The event is free and open to the public',
@@ -119,7 +127,6 @@
     'link' => 'news.html',
     'datetime' => '2018-09-13 11:31'
     ])
-
     
     @include('_partials.article', [
     'heading' => 'Allan Hancock College Renames Health, Science & Mathematics Building in Honor of Marian Regional Medical Center',
@@ -129,7 +136,6 @@
     'link' => 'news.html',
     'datetime' => '2018-09-13 11:31'
     ])
-
         
     @include('_partials.article', [
     'heading' => 'Hancock\'s Winter and Spring Class Schedules Now Available Online; Registration for Both Semesters Starts November 13',
@@ -139,28 +145,28 @@
     'link' => 'news.html',
     'datetime' => '2018-11-09 11:31'
     ])
-
   </div>
 
   </div>
   <div class="col-12 col-md-6">
     <h3 class="bar-bg">Events</h3>
+    <!-- Add option for # of event items to show -->
    <div class="event-feed">
       <article class="post">
-        <div class="post-date"><a href="#"><span class="month">Nov</span><span class="day">26</span></a></div>
-        <div class="post-content"><h4><a href="#">Mars Lander Viewing</a></h4>
+        <div class="post-date"><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-687bfbc4-0168-7c975d8c-00004684demobedework@mysite.edu&recurrenceId="><span class="month">Nov</span><span class="day">26</span></a></div>
+        <div class="post-content"><h4><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-687bfbc4-0168-7c975d8c-00004684demobedework@mysite.edu&recurrenceId=">Mars Lander Viewing</a></h4>
           <p>Join us for the InSight Robotic Lander's historic arrival&hellip; </p>
         </div>
       </article>
       <article class="post">
-          <div class="post-date"><a href="#"><span class="month">Nov</span><span class="day">29</span></a></div>
-          <div class="post-content"><h4><a href="#">Cash for College Workshop</a></h4>
+          <div class="post-date"><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-68ddd583-0168-df0d4b60-000046a5demobedework@mysite.edu&recurrenceId="><span class="month">Nov</span><span class="day">29</span></a></div>
+          <div class="post-content"><h4><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-68ddd583-0168-df0d4b60-000046a5demobedework@mysite.edu&recurrenceId=">Cash for College Workshop</a></h4>
             <p>Get help from financial aid experts with the FAFSA, Dream&hellip; </p>
           </div>
       </article>
       <article class="post">
-          <div class="post-date"><a href="#"><span class="month">Nov</span><span class="day">29</span></a></div>
-          <div class="post-content"><h4><a href="#">Women's Basketball — Cuesta Tournament</a></h4>
+          <div class="post-date"><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-687bfbc4-0168-8237f207-00006f0fdemobedework@mysite.edu&recurrenceId="><span class="month">Nov</span><span class="day">29</span></a></div>
+          <div class="post-content"><h4><a href="http://ec.hancockcollege.edu/cal/event/eventView.do?b=de&calPath=%2Fpublic%2Fcals%2FMainCal&guid=CAL-8aa881b1-687bfbc4-0168-8237f207-00006f0fdemobedework@mysite.edu&recurrenceId=">Women's Basketball — Cuesta Tournament</a></h4>
             <p>vs. TBA @ Cuesta College </p>
           </div>
       </article>
@@ -168,67 +174,59 @@
   </div>
 </div> 
 <p class="pt-5 pb-2">
-  <a class="btn btn-outline-light">More News</a>
-  <a class="btn btn-outline-light">More Events</a>
+  <a href="https://www.hancockcollege.edu/public_affairs/announcements/" class="btn btn-outline-light">More News</a>
+  <a href="http://ec.hancockcollege.edu/cal" class="btn btn-outline-light">More Events</a>
 </p>
+</div>
 @endsection
 
 @section('banner-search')
 <section class="hero left">
-    <div class="hero-image-wrapper jarallax"></div>
-
-<div class="hero-caption delayAnimation">
-       <h2 class="heading">What are you interested in learning?</h2>
-
-
-  <form class="searchform" name="searchform" method="get" action="https://ousearch.omniupdate.com/texis/search/">
-    <input type="hidden" name="pr" value="ahc" />
-    <input type="hidden" name="sufs" value="2" />
-    <div class="form-group">
+  <div class="hero-image-wrapper jarallax"></div>
+  <div class="hero-caption delayAnimation">
+    <h2 class="heading">What are you interested in learning?</h2>
+    <form class="searchform" name="searchform" method="get" action="https://ousearch.omniupdate.com/texis/search/">
+      <input type="hidden" name="pr" value="ahc" />
+      <input type="hidden" name="sufs" value="2" />
+      <div class="form-group">
         <div class="input-group">
-        
-      <label for="program-search-box">Search programs</label>
-      <input name="query" class="queryAutocomplete" id="program-search-box" type="text" placeholder="Search programs of study"  /> 
-      <button type="submit" class="btn btn-primary"><span class="fa fa-search"></span><span class="sr-only">Submit</span></button>
+          <label for="program-search-box">Search programs</label>
+          <input name="query" class="queryAutocomplete" id="program-search-box" type="text" placeholder="Search programs of study" />
+          <button type="submit" class="btn btn-primary"><span class="fa fa-search"></span><span class="sr-only">Submit</span></button>
+        </div>
       </div>
-    </div>
-  </form>
-
-     <p><a href="#" class="more-arrow">View all programs of study</a></p>
-
-       </div>
- </section>
+    </form>
+    <p><a href="#" class="more-arrow">View all programs of study</a></p>
+  </div>
+</section>
 @endsection
 
- 
-
-
 @section ('icons') 
+<!-- Snippet -->
+<div class="row featureicons delayAnimation">
+  <div class="col-12 col-sm-4">
+    @include('_partials.featureicon', [
+    'heading' => '11,500',
+    'desc' => 'Students Enrolled',
+    'link' => '#'
+    ])
 
-<div class="row featureicons delayAnimation" id="results1">
-    <div class="col-12 col-sm-4">
-        @include('_partials.featureicon', [
-        'heading' => '11,500',
-        'desc' => 'Students Enrolled',
-        'link' => '#'
-        ])
-  
-    </div>
-    <div class="col-12 col-sm-4">
-        @include('_partials.featureicon', [
-        'heading' => '100+',
-        'desc' => 'Degrees and Options',
-        'link' => '#'
-        ])
-  
-    </div>
-    <div class="col-12 col-sm-4">
-        @include('_partials.featureicon', [
-        'heading' => '42+',
-        'desc' => 'Clubs and Organization',
-        'link' => '#'
-        ])
-    </div>
+  </div>
+  <div class="col-12 col-sm-4">
+    @include('_partials.featureicon', [
+    'heading' => '100+',
+    'desc' => 'Degrees and Options',
+    'link' => '#'
+    ])
+
+  </div>
+  <div class="col-12 col-sm-4">
+    @include('_partials.featureicon', [
+    'heading' => '42+',
+    'desc' => 'Clubs and Organization',
+    'link' => '#'
+    ])
+  </div>
 </div>
 @endsection
 
@@ -250,9 +248,7 @@
   'class' => ''
   ]) 
 
-
 @endsection
-
 
 @section('locations')  
 true
